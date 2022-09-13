76af693052fc7f81b234c2758525b384a601ebce
eklompus <eklompus@student.21-school.ru>
2022-09-13 22:15:20 +0300

    flags desc

--- src/main/java/ru/twentyone/school/ProcessExecutor.java
+++ src/main/java/ru/twentyone/school/ProcessExecutor.java
@@ -5,20 +5,31 @@ public class ProcessExecutor {
     private final long STEP = 954;
 
     private final long FORCED_STEP = STEP / 2;
+    private long clockCount = 0;
 
-    public long startTime;
+    private boolean run;
 
-    public void init() {
+    private long startTime;
+
+    private Thread thread;
+    public void start() {
         startTime = System.nanoTime();
+        clockCount = 0;
+        thread = new Thread(this::run);
+        thread.start();
+        run = true;
+    }
+
+    public void stop() {
+        run = false;
     }
 
     public void run() {
-        while(true) {
+        while(run) {
             long l = System.nanoTime();
             if (l - startTime > STEP) {
-
-
                 startTime = l;
+                clockCount++;
             }
         }
     }
--- src/main/java/ru/twentyone/school/cpu/Cpu.java
+++ src/main/java/ru/twentyone/school/cpu/Cpu.java
@@ -2,20 +2,24 @@ package ru.twentyone.school.cpu;
 
 import ru.twentyone.school.GBType;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Cpu {
 
-    protected Set<RegisterDesc> registers;
-
+    protected HashMap<RegisterDesc, Integer> registers;
     public void init(GBType type) {
-        registers = new HashSet<>();
+        registers = new HashMap<>();
         for (RegisterDesc value : RegisterDesc.values()) {
             if (value.getTypes().contains(type)) {
-                registers.add(value);
+                registers.put(value, 0);
             }
         }
+    }
+
+    public int getValue(RegisterDesc registerDesc) {
+        return registers.get(registerDesc);
     }
 
 }
